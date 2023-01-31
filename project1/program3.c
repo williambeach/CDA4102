6f50f41d48e2b4be61371cc352a01df123f7127e
williambeach <intratech.cs@icloud.com>
2023-01-30 19:56:04 -0500

    compiled program 3

--- project1/program3.c
+++ project1/program3.c
@@ -7,3 +7,22 @@ Program that takes in names and swaps them and prints them to std output
 *
 */
 
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char name1[50], name2[50], temp[50];
+    printf("Please enter name: ");
+    scanf("%s", name1);
+    printf("\n");
+    printf("Please enter name: ");
+    scanf("%s", name2);
+    printf("\n");
+    printf("Names before swapping: %s %s", name1, name2);
+    printf("\n");
+    strcpy(temp, name1);
+    strcpy(name1, name2);
+    strcpy(name2, temp);
+    printf("Names after swapping: %s %s", name1, name2);
+    return 0;
+}
