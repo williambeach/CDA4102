2e8972f68494a2b26d2c122e6e79c4ebca654422
williambeach <intratech.cs@icloud.com>
2023-04-11 02:23:23 -0400

    add switch case for parsing command line arguments, add 3 helper functions for selection output

--- Project3/BeachWilliam_Project3.c
+++ Project3/BeachWilliam_Project3.c
@@ -7,7 +7,7 @@ CDA4102 - Project 3 - Cache Memory in C
 *
 */
 
-/*Cache = array of sets*/
+//Cache = array of sets
 
 //m = bits per address --> i.e. 32 bit, 64 bit, etc...
 //c = total cache size --> c = s * e * b
@@ -26,34 +26,101 @@ total cache size     block size        associativity
 -c <cache size>   -b <block size>   -e <number of lines>   
 
 type: unified/splitl/splitd     write-back/write-through    write allocate/write no allocate
-      -t [u/sl/sd]                   -w [wb/wt]                      a [wa/wna]
+      -t [u/sl/sd]                   -w [wb/wt]                      -a [wa/wna]
 
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <getopt.h>
+#include <stdlib.h>
 
 struct simulator{
-    u_int64_t cache_size;
-    u_int64_t block_size;
-    u_int16_t num_lines;
+    unsigned int cache_size;
+    unsigned int block_size;
+    unsigned int num_lines;
     char *cache_type;
     char *write_style;
     char *allocation;
 };
 
-
+void cacheType(char *arr);
+void writeType(char *arr);
+void allocationType(char *arr);
 
 int main(int argc, char *argv[]){
-    
-
+    struct simulator cache;
     int option;
     while ((option = getopt(argc, argv, "c:b:e:t:w:a:")) != -1){
-
+        switch (option){
+            case 'c':
+                cache.cache_size = (unsigned int)(atoi(optarg));
+                printf("cache_size: %i bytes\n", cache.cache_size);
+                break;
+            case 'b':
+                cache.block_size = (unsigned int)(atoi(optarg));
+                printf("block_size: %i bytes\n", cache.block_size);
+                break;
+            case 'e':
+                cache.num_lines = (unsigned int)(atoi(optarg));
+                printf("lines per block: %i\n", cache.num_lines);
+                break;
+            case 't':
+                cache.cache_type = optarg;
+                cacheType(cache.cache_type);
+                break;
+            case 'w':
+                cache.write_style = optarg;
+                writeType(cache.write_style);
+                break;
+            case 'a':
+                cache.allocation = optarg;
+                allocationType(cache.allocation);
+                break;
+            default:
+                printf("invalid option\n");
+                printf("aborting program");
+                exit(1);
+        }
     }
-    
 }
 
+void cacheType(char *arr){
+  if (strcmp(arr, "u") == 0){
+    printf("unified cache selected\n");
+  } else if (strcmp(arr, "sl") == 0){
+    printf("split-l cache selected\n");
+  } else if (strcmp(arr, "sd") == 0){
+    printf("split-d cache selected\n");
+  } else{
+    printf("invalid cache type selected\n");
+    printf("aborting program\n");
+    exit(1);
+  }
+}
+      
+void writeType(char *arr){
+  if (strcmp(arr, "wb") == 0){
+    printf("write-back selected\n");
+  } else if (strcmp(arr, "wt") == 0){
+    printf("write-through selected\n");
+  } else{
+    printf("invalid write type selected\n");
+    printf("aborting program\n");
+    exit(1);
+  }
+}
+
+void allocationType(char *arr){
+    if (strcmp(arr, "wa") == 0){
+        printf("write allocate selected\n");
+    } else if (strcmp(arr, "wna") == 0){
+        printf("write no allocate selected\n");
+    } else{
+        printf("invalid allocation selected\n");
+        printf("aborting program\n");
+        exit(1);
+    }
+}
 
 
