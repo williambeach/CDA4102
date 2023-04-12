167767f82033a3d7c255825b83629edf5f69962b
williambeach <intratech.cs@icloud.com>
2023-04-11 21:52:30 -0400

    add hex to binary arrays

--- Project3/BeachWilliam_Project3.c
+++ Project3/BeachWilliam_Project3.c
@@ -11,7 +11,7 @@ CDA4102 - Project 3 - Cache Memory in C
 
 //m = bits per address --> i.e. 32 bit, 64 bit, etc...
 //c = total cache size --> c = s * e * b
-//e = lines per set    --> 2-way set associative (2 lines per set), n-way set associative (n lines per set)
+//e = lines per set --> direct-mapped (1 line per set), 2-way set associative (2 lines per set), n-way set associative (n lines per set)
 //b = bytes of data block per line --> m / 8 bits = bytes per data block in each line
 //s = cache sets       --> c / (e * b)
 //t = tag bits         --> m - (bb + sb)
@@ -22,11 +22,11 @@ CDA4102 - Project 3 - Cache Memory in C
 
 /*
 
-total cache size     block size        associativity  
--c <cache size>   -b <block size>   -e <number of lines>   
+total cache size                       block size                    associativity  
+-c <cache size>                     -b <block size>              -e <number of lines>   
 
-type: unified/splitl/splitd     write-back/write-through    write allocate/write no allocate
-      -t [u/sl/sd]                   -w [wb/wt]                      -a [wa/wna]
+type: unified/split l and d     write-back/write-through     write allocate/write no allocate
+    -t [u/sld]                        -w [wb/wt]                     -a [wa/wna]
 
 */
 
@@ -35,53 +35,59 @@ type: unified/splitl/splitd     write-back/write-through    write allocate/write
 #include <getopt.h>
 #include <stdlib.h>
 
-struct simulator{
-    unsigned int cache_size;
-    unsigned int block_size;
-    unsigned int num_lines;
-    char *cache_type;
-    char *write_style;
-    char *allocation;
-};
+struct settings{
+  unsigned int cache_size;
+  unsigned int block_size;
+  unsigned int num_lines;
+  char *cache_type;
+  char *write_style;
+  char *allocation;
+}cache_settings;
+
+
+
 
 void cacheType(char *arr);
 void writeType(char *arr);
 void allocationType(char *arr);
 
+
 int main(int argc, char *argv[]){
-    struct simulator cache;
-    int option;
-    while ((option = getopt(argc, argv, "c:b:e:t:w:a:")) != -1){
-        switch (option){
-            case 'c':
-                cache.cache_size = (unsigned int)(atoi(optarg));
-                printf("cache_size: %i bytes\n", cache.cache_size);
-                break;
-            case 'b':
-                cache.block_size = (unsigned int)(atoi(optarg));
-                printf("block_size: %i bytes\n", cache.block_size);
-                break;
-            case 'e':
-                cache.num_lines = (unsigned int)(atoi(optarg));
-                printf("lines per block: %i\n", cache.num_lines);
-                break;
-            case 't':
-                cache.cache_type = optarg;
-                cacheType(cache.cache_type);
-                break;
-            case 'w':
-                cache.write_style = optarg;
-                writeType(cache.write_style);
-                break;
-            case 'a':
-                cache.allocation = optarg;
-                allocationType(cache.allocation);
-                break;
-            default:
-                printf("invalid option\n");
-                printf("aborting program");
-                exit(1);
-        }
+  const char hex_to_binary[15] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101",
+  "1110", "1111"};
+  const char hex_numbers = "0123456789abcdef";
+  int option;
+  while ((option = getopt(argc, argv, "c:b:e:t:w:a:")) != -1){
+      switch (option){
+          case 'c':
+              cache_settings.cache_size = (unsigned int)(atoi(optarg));
+              printf("cache_size: %i bytes\n", cache_settings.cache_size);
+              break;
+          case 'b':
+              cache_settings.block_size = (unsigned int)(atoi(optarg));
+              printf("block_size: %i bytes\n", cache_settings.block_size);
+              break;
+          case 'e':
+              cache_settings.num_lines = (unsigned int)(atoi(optarg));
+              printf("lines per block: %i\n", cache_settings.num_lines);
+              break;
+          case 't':
+              cache_settings.cache_type = optarg;
+              cacheType(cache_settings.cache_type);
+              break;
+          case 'w':
+              cache_settings.write_style = optarg;
+              writeType(cache_settings.write_style);
+              break;
+          case 'a':
+              cache_settings.allocation = optarg;
+              allocationType(cache_settings.allocation);
+              break;
+          default:
+              printf("invalid option\n");
+              printf("aborting program");
+              exit(1);
+      }
     }
     //
 }
@@ -89,10 +95,8 @@ int main(int argc, char *argv[]){
 void cacheType(char *arr){
   if (strcmp(arr, "u") == 0){
     printf("unified cache selected\n");
-  } else if (strcmp(arr, "sl") == 0){
-    printf("split-l cache selected\n");
-  } else if (strcmp(arr, "sd") == 0){
-    printf("split-d cache selected\n");
+  } else if (strcmp(arr, "sld") == 0){
+    printf("split-ld cache selected\n");
   } else{
     printf("invalid cache type selected\n");
     printf("aborting program\n");
@@ -116,7 +120,7 @@ void allocationType(char *arr){
     if (strcmp(arr, "wa") == 0){
         printf("write allocate selected\n");
     } else if (strcmp(arr, "wna") == 0){
-        printf("write no allocate selected\n");
+        printf("write no-allocate selected\n");
     } else{
         printf("invalid allocation selected\n");
         printf("aborting program\n");
